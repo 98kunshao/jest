6163dd3b3cbd5c157c1ba753eeeb0ec858ee1ea8
98kunshao <49267366+98kunshao@users.noreply.github.com>
2022-06-29 15:43:44 +0800

    Create utils.ts

---
+++ utils.ts
@@ -0,0 +1,20 @@
+export const getRouteAuthority = (path: string, routeData: Route[]) => {
+  let authorities: string[] | string | undefined;
+  routeData.forEach((route) => {
+    // match prefix
+    if (pathRegexp(`${route.path}/(.*)`).test(`${path}/`)) {
+      if (route.authority) {
+        authorities = route.authority;
+      }
+      // exact match
+      if (route.path === path) {
+        authorities = route.authority || authorities;
+      }
+      // get children authority recursively
+      if (route.routes) {
+        authorities = getRouteAuthority(path, route.routes) || authorities;
+      }
+    }
+  });
+  return authorities;
+};
